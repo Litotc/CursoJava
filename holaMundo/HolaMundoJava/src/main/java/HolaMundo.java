726210359bf0be401d2c7ff8bcfa6f3f65fc82f2
LitoTC <ipup.28@gmail.com>
2021-08-19 15:37:38 -0500

    creamos variable Con String

--- holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
+++ holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
@@ -1,8 +1,19 @@
 
 public class HolaMundo {
     public static void main(String args[]){
+        //definimos  la variable
         int miVariableEntera=10;
         System.out.println(miVariableEntera);
-    }
+        //modificamos el valor de la variable 
+        miVariableEntera=5;
+        System.out.println(miVariableEntera);
+        
+        //crear  variable string
+        String miVariableCadena="saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena="adios";
+        System.out.println(miVariableCadena);
+                }
      
 }
