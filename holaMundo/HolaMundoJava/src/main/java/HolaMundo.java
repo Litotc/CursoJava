7ba24f7c5f1b3c80a8600d5272c45f52bffc65a0
LitoTC <ipup.28@gmail.com>
2021-08-20 22:09:39 -0500

    practica de concatenar

--- holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
+++ holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
@@ -1,26 +1,14 @@
 
 public class HolaMundo {
-    public static void main(String args[]){
-        //definimos  la variable
-        int miVariableEntera=10;
-        System.out.println(miVariableEntera);
-        //modificamos el valor de la variable 
-        miVariableEntera=5;
-        System.out.println(miVariableEntera);
+
+    public static void main(String args[]) {
+        var usuario = "Juan";
+        var titulo = "ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
         
-        //crear  variable string
-        String miVariableCadena="saludos";
-        System.out.println("miVariableCadena = " + miVariableCadena);
-        
-        miVariableCadena="adios";
-        System.out.println("miVariableCadena = " + miVariableCadena); 
-        
-        //usamos el condicinal var para definir cualquier tipo de variable
-        var miVariableEntera2=10;
-        System.out.println("miVariableEntera2 = " + miVariableEntera2); 
-        
-        var miVaribleCadena2="prueba";
-        System.out.println("miVariableEntera2="+miVaribleCadena2);
-                }
-     
-}
+        var i=3;
+        var j=4;
+        System.out.println(i+j);
+
+    }
