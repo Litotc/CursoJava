d03912b3a79f4bea7e4c7baa3b58585b29ec63e2
LitoTC <ipup.28@gmail.com>
2021-08-20 23:53:07 -0500

    tipos numericos primitivos enteros Byte short int long

--- holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
+++ holaMundo/HolaMundoJava/src/main/java/HolaMundo.java
@@ -1,16 +1,28 @@
 
 import java.util.Scanner;
 
-
 public class HolaMundo {
 
     public static void main(String args[]) {
-        System.out.println("Escribe tu nombre");
-        Scanner consola = new Scanner(System.in);
-        var usuario=consola.nextLine();
-        System.out.println("usuario = " + usuario);
-        System.out.println("Escribe el ttítulo");
-        var titulo=consola.nextLine();
-        System.out.println("Resultado:"+titulo+" "+usuario);
-        }
+        /*
+        tipos primitivos enteros: byte, short. int, long
+         */
+        byte numeroByte = 10;
+        short numeroShort = 128;
+        int numeroInt = 1000;
+        long numeroLong=1000000;
+        System.out.println("numeroByte = " + numeroByte);
+        System.out.println("numero minimo byte: " + Byte.MIN_VALUE);
+        System.out.println("numero maximo byte: " + Byte.MAX_VALUE);
+        System.out.println("numeroShort = " + numeroShort);
+        System.out.println("numero minimo short: " + Short.MIN_VALUE);
+        System.out.println("numero maximo short: " + Short.MAX_VALUE);
+        System.out.println("numeroInt = " + numeroInt);
+        System.out.println("numero minimo Int = " + Integer.MIN_VALUE);
+        System.out.println("numero minimo Int = " + Integer.MAX_VALUE);
+        System.out.println("numeroLong = " + numeroLong);
+        System.out.println("numero minimo Long = " + Long.MIN_VALUE);
+        System.out.println("numero minimo Long = " + Long.MAX_VALUE);
+
+    }
 }
